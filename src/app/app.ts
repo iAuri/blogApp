ef6c2c0095e1953ff3531ef5f1baf1c378055b81
Auri <aurii.romero.sanchez@gmail.com>
2025-07-07 20:43:46 +0200

    Agregado lista con 2 noticias estaticas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Blog } from "./components/blog/blog";
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,7 @@ import { Blog } from "./components/blog/blog";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'blogApp';
 }
--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { INoticias } from '../../interfaces/inoticias.interface';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './blog.html',
   styleUrl: './blog.css'
 })
 export class Blog {
+
+  //Nueva noticia
+  nuevaNoticia: INoticias =  {
+    titulo: '',
+    imagen: '',
+    cuerpo: '',
+    fecha: ''
+  }
+
+  //Array de noticias que se muestran en el blog al cargar la página
+  noticias: INoticias[] = [
+    {
+      titulo: 'Primera noticia',
+      imagen: 'https://picsum.photos/id/1/200/200',
+      cuerpo: 'Este es el cuerpo de la primera noticia.',
+      fecha: '2025-01-01'
+    },
+    {
+      titulo: 'Segunda noticia',
+      imagen: 'https://picsum.photos/id/99/200/200',
+      cuerpo: 'Este es el cuerpo de la segunda noticia.',
+      fecha: '2025-01-02'
+    }
+  ]
+
+
 
 }
 
